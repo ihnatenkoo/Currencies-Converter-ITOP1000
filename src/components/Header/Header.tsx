28e440fc44d7d7aee086ce7915176f323749dd0b
Dmytro <ihnatenko.dev@gmail.com>
2022-10-25 17:13:51 +0300

    Add right bar to Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,8 +5,14 @@ import s from './Header.module.scss';
 const Header: FC = () => {
 	return (
 		<header className={s.header}>
-			<span className="material-icons-outlined">monetization_on</span>
-			<h2 className={s.header__title}>Сurrency Exchange</h2>
+			<div className={s.header__leftBar}>
+				<span className="material-icons-outlined">monetization_on</span>
+				<h2 className={s.header__title}>Сurrency Exchange</h2>
+			</div>
+			<div className={s.header__rightBar}>
+				<div>1 UAH = USD</div>
+				<div>1 UAH = EUR</div>
+			</div>
 		</header>
 	);
 };
